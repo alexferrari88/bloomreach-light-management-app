bf07d7bb87bac849a8a2904c95a5a7f1d30a9095
Alex Ferrari <alex@thealexferrari.com>
2025-03-25 11:25:10 +0100

    add file-saver module to fix broken build process

--- client/src/declarations.d.ts
+++ client/src/declarations.d.ts
@@ -1,20 +1,22 @@
 // client/src/declarations.d.ts
 
 // For CSS files
-declare module '*.css' {
+declare module "*.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
 
 // For image files
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.svg';
-declare module '*.gif';
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.gif";
 
 // For static file import
-declare module '*.json' {
+declare module "*.json" {
   const value: any;
   export default value;
 }
+
+declare module "file-saver";
